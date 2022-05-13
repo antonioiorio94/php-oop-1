462beafdc4e6405c77566668583729c7e7628106
Antonio Iorio <antonioiorio94@gmail.com>
2022-05-13 17:19:28 +0200

    aggiunta funzione construct

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ class Movie {
  public $nome;
  public $data_uscita;
  public $genere;
+
+ function __construct($_nome, $_genere, $_data){
+  $this->nome = $_nome;
+  $this->genere = $_genere;
+  $this->data_uscita = $_data;
+}
 }
 
 ?>   
