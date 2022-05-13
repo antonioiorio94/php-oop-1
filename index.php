a12ae4ae0bfe9e9698d39eeb42776d0d05d45e0d
Antonio Iorio <antonioiorio94@gmail.com>
2022-05-13 17:23:59 +0200

    aggiunta istanza

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
 
 class Movie {
- public $nome;
- public $data_uscita;
- public $genere;
+  public $nome;
+  public $data_uscita;
+  public $genere;
 
- function __construct($_nome, $_genere, $_data){
-  $this->nome = $_nome;
-  $this->genere = $_genere;
-  $this->data_uscita = $_data;
+  function __construct($_nome, $_genere, $_data){
+    $this->nome = $_nome;
+    $this->genere = $_genere;
+    $this->data_uscita = $_data;
+  }
 }
-}
+
+//inizializzo istanza
+
+$film_disponibile_1 = new Movie('Pirati dei Caraibi', 'Avventura', '2018');
+
+var_dump($film_disponibile_1);
 
 ?>   
